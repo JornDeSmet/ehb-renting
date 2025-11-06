5a24cf3a160e2f0ded222ab960633edb92ecdbec
jornDS <117446313+JornDeSmet@users.noreply.github.com>
2025-11-06 18:53:43 +0100

    setting up pom file and database

---
+++ src/main/java/com/example/ehbrenting/EhbRentingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ehbrenting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EhbRentingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EhbRentingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ehbrenting/EhbRentingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ehbrenting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhbRentingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
